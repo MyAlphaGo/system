e01ecbbe3152db781bb37263fc78e8592cab9f5c
曹林 <caolin@xiaoduotech.com>
2020-11-23 19:25:24 +0800

    feat: 完善api

--- src/api/index.js
+++ src/api/index.js
@@ -40,12 +40,78 @@ export class PositionService {
   static async getPositionList(params = {}) {
     return get('/position/list', params)
   }
+  static async getPositionInfo(params = {}) {
+    return get('/position/user_info', params)
+  }
+  static async createPosition(params) {
+    return post('/position/create', params)
+  }
+  static async editPosition(params) {
+    return post('/position/edit', params)
+  }
+  static async delPosition(params) {
+    return del('/position/delete', params)
+  }
 }
 
 // 部门相关
 export class DeptService {
   static async getDeptList(params = {}) {
     return get('/dept/list', params)
+  }
+  static async getDeptTree(params) {
+    return del('/dept/tree', params)
+  }
+  static async getDeptInfo(params = {}) {
+    return get('/dept/user_info', params)
+  }
+  static async createDept(params) {
+    return post('/dept/create', params)
+  }
+  static async editDept(params) {
+    return post('/dept/edit', params)
+  }
+  static async delDept(params) {
+    return del('/dept/delete', params)
+  }
+
+}
+
+// 培训相关
+export class TrainService {
+  static async getTrainInfo(params = {}) {
+    return get('/train/user_info', params)
+  }
+  static async createTrain(params) {
+    return post('/train/create', params)
+  }
+  static async editTrain(params) {
+    return post('/train/edit', params)
+  }
+  static async delTrain(params) {
+    return del('/train/delete', params)
+  }
+  static async getTrainList(params) {
+    return get('/train/list', params)
+  }
+}
+
+// 社保相关
+export class SocialService {
+  static async getSocialInfo(params = {}) {
+    return get('/social/user_info', params)
+  }
+  static async createSocial(params) {
+    return post('/social/create', params)
+  }
+  static async editSocial(params) {
+    return post('/social/edit', params)
+  }
+  static async delSocial(params) {
+    return del('/social/delete', params)
+  }
+  static async getSocialList(params) {
+    return get('/social/list', params)
   }
 }
 export default UserService
